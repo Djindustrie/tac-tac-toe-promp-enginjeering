e453ab20eca45be90583fe2c9bb0e92f84e67f94
Djindustrie <Djindustrie@Protonmail.com>
2024-07-17 13:14:10 +0200

    Add circle

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function render() {
         for (let j = 0; j < 3; j++) {
             let index = i * 3 + j;
             let cellValue = fields[index];
-            let displayValue = cellValue === 'circle' ? 'o' : cellValue === 'cross' ? 'x' : '';
+            let displayValue = cellValue === 'circle' ? generateCircleSVG() : cellValue === 'cross' ? 'x' : '';
             tableHTML += `<td>${displayValue}</td>`;
         }
         tableHTML += '</tr>';
@@ -24,3 +24,16 @@ function render() {
     contentDiv.innerHTML = tableHTML;
 }
 
+function generateCircleSVG() {
+    const svgHTML = `
+        <svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="35" cy="35" r="30" stroke="#00B0EF" stroke-width="5" fill="none">
+                <animate attributeName="stroke-dasharray" from="0, 188.4" to="188.4, 188.4" dur="400ms" fill="freeze" />
+            </circle>
+        </svg>
+    `;
+    return svgHTML;
+}
+
+// Beispiel: Füge den SVG-Code zu einem div mit der ID 'Content' hinzu
+document.getElementById('content').innerHTML = generateCircleSVG();
